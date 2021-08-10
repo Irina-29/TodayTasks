d7741ca49c9207a790f19b5d8d8efc7947879a59
Irina-29 <irina.barbu229@gmail.com>
2021-08-10 17:11:31 +0300

    update

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-// tasks: const task = {name: "", priority: ""}; 
-//     name: value from textarea
-//     priority: value from select
-
 // cand toate taskurile sunt completate:
 //     variabila care numara cate taskuri sunt in lista de abia dupa ce este inserat 1 task; 
 //     cand variabila == 0 (nu mai sunt taskuri in lista):
 //          -> taskurile din lista de completed sunt sterse 
 //          -> mesaj congratulations
+
+var tasks = [];
+
+var len = 0;
+
+function addTask() {
+    var taskName = document.getElementById("floatingTextarea").value;
+    var taskPriority = document.getElementById("priorityOption").value;
+    tasks.push({
+        name: taskName,
+        priority: taskPriority
+    });
+    len++;
+}
