6a10ccbb9e736a6395a0ff1b2ac3fbbf6c41be31
Irina-29 <irina.barbu229@gmail.com>
2021-08-11 16:20:02 +0300

    update

--- main.js
+++ main.js
@@ -6,16 +6,38 @@
 
 // sort by priority: option value devine numar de la 1 la 3 (cu switch statement) -> sortare
 
-var tasks = [];
 
-var len = 0;
+// method 1 - using class for tasks
+class task {
+    constructor(name, priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+}
 
 function addTask() {
     var taskName = document.getElementById("floatingTextarea").value;
     var taskPriority = document.getElementById("priorityOption").value;
-    tasks.push({
-        name: taskName,
-        priority: taskPriority
-    });
-    len++;
+    var newTask = new task(taskName, taskPriority);
+    console.log(newTask);
 }
+
+
+// method 2 - using array of objects for tasks
+// var task = [];
+
+// var len = 0;
+
+// function addTask() {
+//     var taskName = document.getElementById("floatingTextarea").value;
+//     var taskPriority = document.getElementById("priorityOption").value;
+//     task.push({
+//         name: taskName,
+//         priority: taskPriority
+//     });
+//     var li = document.createElement('li');
+//     li = task[len].name;
+//     document.getElementById("tasks-list").innerHTML = li;
+//     console.log(task[len]);
+//     len++;
+// }
