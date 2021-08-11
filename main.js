59c07a8d58f06770440ffe1a6e47622477ca0afd
Irina-29 <irina.barbu229@gmail.com>
2021-08-11 17:49:04 +0300

    update

--- main.js
+++ main.js
@@ -7,37 +7,42 @@
 // sort by priority: option value devine numar de la 1 la 3 (cu switch statement) -> sortare
 
 
-// method 1 - using class for tasks
-class task {
-    constructor(name, priority) {
-        this.name = name;
-        this.priority = priority;
-    }
-}
-
-function addTask() {
-    var taskName = document.getElementById("floatingTextarea").value;
-    var taskPriority = document.getElementById("priorityOption").value;
-    var newTask = new task(taskName, taskPriority);
-    console.log(newTask);
-}
-
-
-// method 2 - using array of objects for tasks
-// var task = [];
-
-// var len = 0;
+// method - using class for tasks
+// class task {
+//     constructor(name, priority) {
+//         this.name = name;
+//         this.priority = priority;
+//     }
+// }
 
 // function addTask() {
 //     var taskName = document.getElementById("floatingTextarea").value;
 //     var taskPriority = document.getElementById("priorityOption").value;
-//     task.push({
-//         name: taskName,
-//         priority: taskPriority
-//     });
-//     var li = document.createElement('li');
-//     li = task[len].name;
-//     document.getElementById("tasks-list").innerHTML = li;
-//     console.log(task[len]);
-//     len++;
+//     var newTask = new task(taskName, taskPriority);
+//     console.log(newTask);
 // }
+
+
+// method - using array of objects for tasks
+var task = [], len = 0;
+
+function addTask() {
+
+    var taskName = document.getElementById("floatingTextarea").value;
+    var taskPriority = document.getElementById("priorityOption").value;
+
+    task.push({
+        name: taskName,
+        priority: taskPriority
+    });
+
+    var ul = document.getElementById("tasks-list");
+    var li = document.createElement('li');
+    li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center")
+
+    li.appendChild(document.createTextNode(task[len].name));
+    ul.appendChild(li);
+
+    console.log(task[len]);
+    len++;
+}
